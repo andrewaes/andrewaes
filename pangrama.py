4a64e23c2bc1064bd981476127ad7c12e61bd5e8
andrewaes <99044112+andrewaes@users.noreply.github.com>
2022-06-13 20:23:31 -0300

    Add files via upload
    verifica se uma frase é um pangrama

---
+++ pangrama.py
@@ -0,0 +1,12 @@
+lista = ["a", "b" ,"c" ,"d" ,"e" ,"f" ,"g" ,"h" ,"i" ,"j" ,"l" ,"m" ,"n" ,"o", "p", "q", "r", "s", "t", "u", "v" ,"x", "z"]
+
+pangrama = "gazeta publica hoje: breve anuncio de faxina na quermesse"
+
+qtd_letras = 0
+
+for i in lista[0:]:
+    if i in pangrama:
+        qtd_letras += 1
+print(qtd_letras)
+
+        
